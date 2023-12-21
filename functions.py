06f90b8865ad3f40997f2308c30a4fa726008fd8
Caio Fernando <79061428+C41m@users.noreply.github.com>
2023-12-20 21:44:02 -0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,34 +1,239 @@
 import os
 import streamlit as st
-import google.oauth2.credentials
+from pytube import YouTube, Playlist
+from datetime import datetime, timedelta
+import re
+
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
+class VideoCache:
+    def __init__(self):
+        self.cache = {}
+
+    def get_audio_streams_abr(self, video_url):
+        if video_url in self.cache:
+            print("Usando cache para", video_url)
+            return self.cache[video_url]
+        else:
+            print("Chamando API para", video_url)
+            try:
+                video = YouTube(video_url)
+                # Obter todas as transmissões de áudio disponíveis
+                audio_streams = video.streams.filter(only_audio=True).order_by('abr')
+                # Obter apenas as informações dentro do campo abr
+                abr_list = list([stream.abr for stream in audio_streams])
+                # Inverter a lista para melhor qualidade primeiro
+                abr_list = list(reversed(abr_list))
+                # Armazenar no cache
+                self.cache[video_url] = abr_list, audio_streams
+                return abr_list, audio_streams
+            
+            except Exception as e:
+                print(f"Erro ao obter transmissões de áudio: {e}")
+                return []
+
+video_cache = VideoCache()
+
 # Função para limpar arquivos temporários
 def cleanup_temp_files(directory):
-    for file_name in os.listdir(directory):
-        file_path = os.path.join(directory, file_name)
-        try:
-            if os.path.isfile(file_path):
-                os.unlink(file_path)
-        except Exception as e:
-            st.warning(f"Não foi possível excluir o arquivo {file_path}. Erro: {e}")
+    try:
+        # Excluir todos os arquivos dentro da pasta
+        for file_name in os.listdir(directory):
+            file_path = os.path.join(directory, file_name)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                st.warning(f"Não foi possível excluir o arquivo {file_path}. Erro: {e}")
+
+        # Excluir a pasta vazia
+        os.rmdir(directory)
+
+    except Exception as e:
+        st.warning(f"Não foi possível excluir a pasta {directory}. Erro: {e}")
+
+def get_video_details(video_url):
+    video = YouTube(video_url)
+    video_id = video.video_id
+    youtube = build('youtube', 'v3', developerKey='AIzaSyDFq1AMiBfEfOe2g9vC6FU4tJhzySgJk9A')
+    request = youtube.videos().list(part='contentDetails,snippet,statistics,player', id=video_id)
+    response = request.execute()
+
+    # Titulo do Vídeo
+    title = response['items'][0]['snippet']['title']
+
+    # Duração do Vídeo
+    duration_iso = response['items'][0]['contentDetails']['duration']
+    duration = format_duration(duration_iso)
+
+    # Thumb do Vídeo
+    thumbnail_url = response['items'][0]['snippet']['thumbnails']['maxres']['url']
+
+    # Qualidades do Vídeo
+    abr_list, audio_streams = video_cache.get_audio_streams_abr(video_url)
+
+    return title, duration, thumbnail_url, abr_list, audio_streams
+    
+def format_duration(duration_iso):
+    try:
+        # Remova os caracteres PT da string
+        duration = duration_iso.replace("PT", "")
+
+        # Converta para um objeto timedelta
+        delta = timedelta()
+
+        # Iterar sobre as partes da string e adicionar à timedelta
+        for part in duration.split("M"):
+            if "H" in part:
+                delta += timedelta(hours=int(part.split("H")[0]))
+                part = part.split("H")[1]
+            if "S" in part:
+                delta += timedelta(seconds=int(part.split("S")[0]))
+                part = part.split("S")[1]
+            if part:
+                delta += timedelta(minutes=int(part))
+
+        # Formate a timedelta como string no formato HH:MM:SS
+        hours, remainder = divmod(delta.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        formatted_duration = "{:02}:{:02}:{:02}".format(hours, minutes, seconds)
+
+        return formatted_duration
+
+    except Exception as e:
+        return {"error": str(e)}
+
+def get_video_streams(video_url):
+    try:
+        video = YouTube(video_url)
+
+        # Obter todas as transmissões de vídeo disponíveis
+        video_streams = video.streams.filter(file_extension="mp4").order_by('resolution')
+
+        return video_streams
+
+    except Exception as e:
+        print(f"Erro ao obter transmissões de vídeo: {e}")
+        return []
+
+def mp3_converter(file_download):
+
+    base, ext = os.path.splitext(file_download)
+    new_file = base + '.mp3'
+
+    if os.path.exists(new_file):
+        os.remove(new_file)
+
+    os.rename(file_download, new_file)
+    return new_file
+
+def file_rename(file_download, title):
+
+    title = title.replace('/', '')
+    title = title.replace('|', '')
+    title = title.replace(':', '')
+    title = title.replace('"', '')
+    title = title.replace("'", '')
+    title = title.replace("*", '')
+    title = title.replace("?", '')
 
 
-def authenticate_youtube():
-    # Obtenha o caminho absoluto do diretório atual onde o script está sendo executado
-    script_directory = os.path.dirname(os.path.abspath(__file__))
+    # Construir o novo caminho do arquivo com o mesmo diretório
+    new_file = os.path.join(os.path.dirname(file_download), f"{title}.mp3")
 
-    # Combine o caminho do diretório com o nome do arquivo
-    CLIENT_SECRETS_FILE = os.path.join(script_directory, "client_secret_999265794631-98539690rmnr37pm89ikd4rdknuomma5.apps.googleusercontent.com.json")
+    # Renomear o arquivo
+    os.rename(file_download, new_file)
+    return new_file
 
-    # Substitua 'path/to/client_secrets.json' pelo caminho real do seu arquivo JSON de credenciais
-    SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
+def get_playlist_details(playlist_url, progress_bar1):
+    playlist = Playlist(playlist_url)
+    playlist_id = playlist.playlist_id
+    playlist_urls = playlist.video_urls
+    youtube = build('youtube', 'v3', developerKey='AIzaSyDFq1AMiBfEfOe2g9vC6FU4tJhzySgJk9A')
+    # request = youtube.playlistItems().list(part='contentDetails,id,snippet,status', playlistId=playlist_id)
+    # response = request.execute()
 
-    # Configurar o fluxo de autenticação
-    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-    credentials = flow.run_local_server(port=0)
 
-    # Criar um objeto de serviço do YouTube autenticado
-    youtube = build("youtube", "v3", credentials=credentials)
-    return youtube, script_directory
+    all_playlist_items = []
+    def process_items(items):
+        for item in items:
+            try:
+                video_id = item['contentDetails']['videoId']
+                privacy_status = item['status']['privacyStatus']
+
+                # Considera apenas vídeos públicos
+                if video_id and privacy_status == 'public':
+                    all_playlist_items.append(item)
+            except (KeyError, TypeError):
+                pass  # Ignora itens que não têm os campos necessários
+
+        
+    # Sua chamada inicial
+    request = youtube.playlistItems().list(part='contentDetails,id,snippet,status', playlistId=playlist_id, maxResults=50)
+    response = request.execute()
+    process_items(response['items'])
+    
+    # Verifica se há mais páginas
+    while 'nextPageToken' in response:
+        request = youtube.playlistItems().list(part='contentDetails,id,snippet,status', playlistId=playlist_id, pageToken=response['nextPageToken'], maxResults=50)
+        response = request.execute()
+        # Processa os próximos 50 itens
+        process_items(response['items'])
+        print('Resposta')
+
+    
+    request_title = youtube.playlists().list(part='contentDetails,id,snippet,status', id=playlist_id)
+    response_title = request_title.execute()
+
+    playlist_title = response_title['items'][0]['snippet']['title']
+    
+    video_ids = []
+    for item in all_playlist_items:
+        content_details = item.get('contentDetails', {})
+        video_id = content_details.get('videoId', None)
+        if video_id:
+            video_ids.append(video_id)
+        else:
+            # Aqui você pode lidar com a situação em que 'videoId' não está disponível
+            # Pode adicionar uma ID padrão, pular o item, etc.
+            print('Erro aqui')  
+
+    print('Peguei ID')
+
+    video_titles = [item['snippet']['title'] for item in all_playlist_items]
+
+    # Capturar Thumb
+    video_thumbs = []
+    for item in all_playlist_items:
+        snippet = item.get('snippet', {})
+        thumbnails = snippet.get('thumbnails', {})
+
+        # Tentar pegar as resoluções em uma ordem específica
+        for resolution in ['maxres', 'standard', 'high', 'medium', 'default']:
+            thumbnail_info = thumbnails.get(resolution, {})
+            url = thumbnail_info.get('url', None)
+            
+            if url:
+                video_thumbs.append(url)
+                break  # Se encontrar uma URL válida, sai do loop
+            elif resolution == 'default':
+                # Se todas as resoluções falharem, pode adicionar uma URL padrão ou fazer outra manipulação
+                video_thumbs.append("Vídeo sem Thumb")
+
+    # Capturar a duração do vídeo
+    duration = []
+    for i, (video_id) in enumerate(video_ids, 1):
+        request = youtube.videos().list(part='contentDetails,snippet,statistics,player', id=video_id)
+        response_video = request.execute()
+        print(video_id)
+        print(response_video)
+
+        # Duração do Vídeo
+        duration_iso = response_video['items'][0]['contentDetails']['duration']
+        duration.append(format_duration(duration_iso))
+        
+        progress_bar1.progress(i / len(video_ids))
+    progress_bar1.empty()
+
+    return video_ids, video_titles, video_thumbs, playlist_title, duration, playlist_urls, progress_bar1
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import locale
 import os
-from functions import cleanup_temp_files, authenticate_youtube
+from functions import cleanup_temp_files, get_video_details, get_playlist_details, file_rename
 from pytube import YouTube
 from pytube import Playlist
 from zipfile import ZipFile
@@ -9,59 +9,6 @@ import tempfile
 
 
 
-def video_info_func():
-    # Pegando título do vídeo
-    title = video.title
-    author = video.author
-    full_title = author + ' - ' + title
-    
-    # Pegando duração do vídeo
-    duration_seconds = video.length
-    duration_minutes = divmod(duration_seconds, 60)
-    duration_formatted = f"{duration_minutes[0]}:{duration_minutes[1]}"
-    return full_title, duration_formatted
-
-def file_rename(file_download):
-    full_title, duration_formatted = video_info_func()
-
-    # Construir o novo caminho do arquivo com o mesmo diretório
-    new_file = os.path.join(os.path.dirname(file_download), f"{full_title}.mp3")
-
-    # Renomear o arquivo
-    os.rename(file_download, new_file)
-
-def mp3_converter(file_download):
-    base, ext = os.path.splitext(file_download)
-    new_file = base + '.mp3'
-
-    if os.path.exists(new_file):
-        os.remove(new_file)
-
-    os.rename(file_download, new_file)
-    return new_file
-
-def download_playlist_func(video):
-    audio_streams = video.streams.filter(only_audio=True)
-
-    # Extrair as taxas de bits disponíveis para as streams de áudio
-    available_bitrates = [int(stream.abr.replace('kbps', '')) for stream in audio_streams]
-
-    # Obter a maior taxa de bits disponível
-    highest_bitrate = max(available_bitrates)
-    
-    # Encontrar a stream correspondente à taxa de bits mais alta
-    selected_stream = None
-    for stream in audio_streams:
-        if int(stream.abr.replace('kbps', '')) == highest_bitrate:
-            selected_stream = stream
-            break
-
-    # Realizar o download da stream de áudio escolhida
-    if selected_stream:
-        file_download = selected_stream.download(output_path=temp_dir)
-        #mp3_converter(file_download)
-        file_rename(file_download)
-        st.success(f"Download {index} concluído com sucesso!")
 
 def zip_musics_func():
     # Diretório dos downloads
@@ -78,18 +25,8 @@ def zip_musics_func():
                 arcname = os.path.relpath(file_path, download_folder)
                 zipf.write(file_path, arcname=arcname)
 
+    return zip_file_path
 
-    youtube, script_directory = authenticate_youtube()
-    CLIENT_SECRETS_FILE = os.path.join(script_directory, "client_secret_999265794631-98539690rmnr37pm89ikd4rdknuomma5.apps.googleusercontent.com.json")
-    youtube = authenticate_youtube()
-
-    # Exibir o link para download do arquivo ZIP
-    st.info("Clique abaixo para baixar todos os arquivos compactados:")
-    with open(zip_file_path, "rb") as zip_file:
-        st.download_button(label="Baixar Arquivos Compactados", data=zip_file, key="download_button", file_name="download.zip")
-
-    # Remover o arquivo ZIP temporário após o download
-    os.remove(zip_file_path)
 
 
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
@@ -115,105 +52,109 @@ with st.container():
     # Adiciona uma mensagem para pressionar Enter
     enter_message = st.info("Pressione Enter para continuar.")
 
-
-
-
 if link:
-    try:
-        # Obter informações do vídeo
-        video = YouTube(link)
-
-        # Remover a mensagem de "Pressione Enter para continuar."
-        enter_message.empty()
-
-        # Filtrar apenas as streams de áudio
-        audio_streams = video.streams.filter(only_audio=True)
-
-        # Extrair as taxas de bits disponíveis para as streams de áudio
-        available_bitrates = [stream.abr for stream in audio_streams]
-
-        # Remover o "kbps" e converter para inteiros (ignorando valores que não podem ser convertidos)
-        int_bitrates = [int(b.replace('kbps', '')) for b in available_bitrates if b.replace('kbps', '').isdigit()]
-
-        # Reverter a lista para exibir de maior para menor
-        reversed_bitrates = sorted(int_bitrates, reverse=True)
-
-        # Converter novamente para strings para exibir no seletor
-        reversed_bitrates_str = [f"{b}kbps" for b in reversed_bitrates]
-
-        # Exibir um seletor para escolher a taxa de bits
-        selected_bitrate = st.radio("Escolha a qualidade de áudio (Taxa de Bits):", reversed_bitrates_str)
-
-        # Inicializar a variável para armazenar a stream selecionada
-        selected_stream = None
+    title, duration, thumbnail_url, abr_list, audio_streams  = get_video_details(link)
+    
+    with st.container():
+        st.image(thumbnail_url, width=450, use_column_width=True)
+        st.markdown(f'### {title}')
+        st.markdown(f'Duração: {duration}')
+        
+        selected_bitrate_radio = st.radio('Escolha a qualidade de áudio (Taxa de Bits):', abr_list)
 
         # Encontrar a stream de áudio correspondente à taxa de bits escolhida
+        selected_stream = None
         for stream in audio_streams:
-            if stream.abr == selected_bitrate:
+            if stream.abr == selected_bitrate_radio:
                 selected_stream = stream
                 break
-
-        # Verificar se o botão de download foi pressionado
-        if st.button("Selecionar"):
-
+        
+        conv_btn = st.button("Converter", key='convBtnVideo')
+        if conv_btn:
             # Realizar o download da stream de áudio escolhida
-              if selected_stream:
+            if selected_stream:
+                #file_download = selected_stream.download()
                 file_download = selected_stream.download(output_path=temp_dir)
-                file_ok = mp3_converter(file_download)
-                st.success("Download concluído com sucesso!")
+                file_ok = file_rename(file_download, title)
+                st.success("Conversão concluída!")
                 
                 # Adicionar um botão para baixar o arquivo
                 with open(file_ok, 'rb') as file:
-                    st.download_button(label='Baixar Vídeo', data=file.read(), key='download_button', file_name=os.path.basename(file_ok))
+                    download_btn = st.download_button(label='Download', data=file.read(), key='download_button', file_name=os.path.basename(file_ok))
                 
+                if download_btn:
+                    st.success('Download concluído com sucesso!')
+
                 # Limpar arquivos temporários após o download
                 cleanup_temp_files(temp_dir)
 
+            else:
+                st.error('Erro: Stream não encontrada')
 
-    except Exception as e:
-        st.error(f"Ocorreu um erro: {e}")
+    st.markdown('---')
 
+
+st.markdown('---')
 with st.container():
-    link = st.text_input('Insira o link da playlist do Youtube:', placeholder='Cole o link aqui')
+    #if 'enter_message' not in st.session_state:
+        #st.session_state.enter_message = st.info("Pressione Enter para continuar.")
+
+    with st.container():
+        link = st.text_input('Insira o link da playlist do Youtube:', placeholder='Cole o link aqui') 
+        conv_btn_playlist = st.button("Converter!", key='convBtnPlaylist')
+        st.session_state.btn_down = st.markdown('')
+
     
-    # Adiciona uma mensagem para pressionar Enter
-    enter_message = st.info("Pressione Enter para continuar.")
+    if link and conv_btn_playlist:
+        cleanup_temp_files(temp_dir)
+        st.session_state.enter_message.empty()  # Limpar a mensagem
+        st.session_state.enter_message = st.info('Carregando...')
+        progress_bar1 = st.progress(0)
+        video_ids, video_titles, video_thumbs, playlist_title, duration, playlist_urls, progress_bar1 = get_playlist_details(link, progress_bar1)
+        
+        progress_bar = st.progress(0)
+        st.session_state.enter_message.info('Convertendo...')
+        
+        st.markdown(f'# {playlist_title}')
+        for i, (video_name, video_id, video_thumb, duration, video_url) in enumerate(zip(video_titles, video_ids, video_thumbs, duration, playlist_urls), 1):
 
-    # Verificar se o botão de download foi pressionado
-    if st.button("Baixar Playlist"):
+            progress_bar.progress(i / len(video_titles))
+            st.session_state.enter_message.info(f'Convertendo: {i}/{len(video_titles)} videos')
 
-        # Realizar o download de vídeos da playlist
-        playlist = Playlist(link)
-        st.markdown(f'# Título da Playlist: {playlist.title}')
+            with st.container():
+                col1, col2 = st.columns(2)   
+                with col1:
+                    if video_thumb:
+                        st.image(video_thumb, width=450, use_column_width=True)
+                with col2:
+                    st.markdown(f'#### {video_name}')
+                    st.markdown(f'##### Duração: {duration}')
 
-        videos = playlist.video_urls
+                    video = YouTube(video_url)
+                    # Obter a primeira stream de áudio disponível
+                    audio_stream = video.streams.filter(only_audio=True).order_by('abr').last()
+                    if audio_stream:
+                        file_download = audio_stream.download(output_path=temp_dir)
+                        file_ok = file_rename(file_download, video_name)
+                        #cleanup_temp_files(temp_dir)
+                        st.success(f'Conversão do vídeo {i}/{len(video_titles)} concluído!')
+            st.markdown('---')
 
-        for index, url in enumerate(videos, start=1):
-            video = YouTube(url)
+        # Adicionar o botão de download após o progresso
+        with st.container():
+            # Compactar todas as músicas
+            zip_file_path = zip_musics_func()
+            # Exibir o link para download do arquivo ZIP
+            st.info("Clique abaixo para baixar todos os arquivos compactados:")
+            with open(zip_file_path, "rb") as zip_file:
+                st.session_state.btn_down.download_button(label="Download", data=zip_file, key="download_button1", file_name="download.zip")
             
             #
-            st.write(f'Vídeo {index}/{len(videos)}')
-            col1, col2 = st.columns(2)
-            with col1:
-                st.image(video.thumbnail_url, width=300, use_column_width=True, )
-            with col2:
-                full_title, duration_formatted = video_info_func()
-                st.markdown(f'Nome do Arquivo: {full_title}')
-                st.markdown(f'Duração: {duration_formatted}')
-                st.markdown(f'Link: {url}')
-                
-                with st.spinner('Baixando...'):
-                    download_playlist = download_playlist_func(video)
-                    
-            st.markdown('---')
-        
-        # Compactar todas as músicas
-        zip_musics = zip_musics_func()
+            st.session_state.enter_message.success('Concluído! Aperte em Download!')
 
-        # Limpar arquivos temporários após o download
-        cleanup_temp_files(temp_dir)
+            # Remover o arquivo ZIP temporário após o download
+            os.remove(zip_file_path)
 
-        # Remover a mensagem de "Pressione Enter para continuar."
-        enter_message.empty()
+            # # Limpar arquivos temporários após o download
+            cleanup_temp_files(temp_dir)
 
-        st.success("Download da playlist concluído com sucesso!")
